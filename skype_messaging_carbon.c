e089cb9bdb07f3f2bd4a194494676a6fb1ae957e
Eion Robb <eion@robbmob.com>
2007-11-18 21:29:34 +0000

    Bringing through OSX changes

--- skype_messaging_carbon.c
+++ skype_messaging_carbon.c
@@ -3,24 +3,35 @@
 #include <glib.h>
 #include <CoreFoundation/CoreFoundation.h>
 
+#include "AutoreleasePoolInit.h"
+
+//change this to 0 if using an old version of the Skype.framework
 #define SENDSKYPERETURNS 1
 
 static gboolean connected_to_skype = FALSE;
 
-void SkypeNotificationReceived(CFStringRef input)
+void
+SkypeNotificationReceived(CFStringRef input)
 {
-	printf("Message received\n");
-	char *output = CFStringGetCStringPtr(input, kCFStringEncodingUTF8);
+	char *output = NULL;
+	void *pool = initAutoreleasePool();
+	int strlen = CFStringGetMaximumSizeForEncoding(CFStringGetLength(input), kCFStringEncodingUTF8);
+	
+	printf("Message received");
+	output = (char *)CFStringGetCStringPtr(input, kCFStringEncodingUTF8);
 	if (!output)
 	{
-		output = NewPtr(CFStringGetLength(input)+1);
-		CFStringGetCString(input, output, CFStringGetLength(input)+1, kCFStringEncodingUTF8);
+		output = NewPtr(strlen+1);
+		CFStringGetCString(input, output, strlen+1, kCFStringEncodingUTF8);
 	}
-	
+	printf(" %s\n", output);
 	g_thread_create((GThreadFunc)skype_message_received, (void *)output, FALSE, NULL);
+	
+	destroyAutoreleasePool(pool);
 }
 
-void SkypeAttachResponse(unsigned int aAttachResponseCode)
+void
+SkypeAttachResponse(unsigned int aAttachResponseCode)
 {
 	if (aAttachResponseCode)
 	{
@@ -34,13 +45,15 @@ void SkypeAttachResponse(unsigned int aAttachResponseCode)
 	}
 }
 
-void SkypeBecameAvailable(CFPropertyListRef aNotification)
+void
+SkypeBecameAvailable(CFPropertyListRef aNotification)
 {
 	printf("Skype became available\n");
 	connected_to_skype = TRUE;
 }
 
-void SkypeBecameUnavailable(CFPropertyListRef aNotification)
+void
+SkypeBecameUnavailable(CFPropertyListRef aNotification)
 {
 	printf("Skype became unavailable\n");
 	connected_to_skype = FALSE;
@@ -54,25 +67,33 @@ static struct SkypeDelegate skypeDelegate = {
 	SkypeBecameUnavailable
 };
 
-static gboolean skype_connect_thread(gpointer data)
+static gboolean
+skype_connect_thread(gpointer data)
 {
+	void *pool = initAutoreleasePool();
+
 	printf("Start inner event loop\n");
 	RunApplicationEventLoop();
 	printf("End of event loop\n");
 	
+	destroyAutoreleasePool(pool);
+	
+	//don't loop this thread
 	return FALSE;
 }
 
-static gboolean skype_connect()
+static gpointer static_pool;
+
+static gboolean
+skype_connect()
 {
-	EventRef theEvent;
-	EventTargetRef theTarget;
-	EventRecord *eventRecord;
 	gboolean is_skype_running = FALSE;
+	
+	static_pool = initAutoreleasePool();
 	
 	is_skype_running = IsSkypeRunning();
 	
-	printf("Is Skype running? '%s'\n", is_skype_running?"Yes":"No");
+	printf("Is Skype running? '%s'\n", (is_skype_running?"Yes":"No"));
 	if (!is_skype_running)
 		return FALSE;
 		
@@ -88,22 +109,29 @@ static gboolean skype_connect()
 	return TRUE;
 }
 
-static void skype_disconnect()
+static void
+skype_disconnect()
 {
+	connected_to_skype = FALSE;
 	DisconnectFromSkype();
 	RemoveSkypeDelegate();
 	RunCurrentEventLoop(1);
+	
+	destroyAutoreleasePool(static_pool);
 }
 
-static void send_message(char* message)
+static void
+send_message(char* message)
 {
+	gpointer pool = initAutoreleasePool();
 	printf("Skype send message\n");
 #if SENDSKYPERETURNS
-	CFStringRef returnString;
+	CFStringRef returnString = NULL;
 	returnString = SendSkypeCommand(CFStringCreateWithCString(NULL, message, kCFStringEncodingUTF8));
 	if (returnString)
 		SkypeNotificationReceived(returnString);
 #else
 	SendSkypeCommand(CFStringCreateWithCString(NULL, message, kCFStringEncodingUTF8));
 #endif
+	destroyAutoreleasePool(pool);
 }
