5df7db5c6afe5d0e7c7393665a427aea202259d0
Eion Robb <eion@robbmob.com>
2008-03-02 03:15:39 +0000

    Added replacement debug functions that are thread safe

---
+++ debug.c
@@ -0,0 +1,81 @@
+#include <glib.h>
+#include <debug.h>
+
+typedef struct
+{
+	PurpleDebugLevel level;
+	char *category;
+	char *message;
+} SkypeDebugWrapper
+
+void skype_debug(PurpleDebugLevel level, const char *category,
+				const char *format, ...);
+
+void skype_debug_info(const gchar *category, const gchar *format, ...);
+void skype_debug_warning(const gchar *category, const gchar *format, ...);
+void skype_debug_error(const gchar *category, const gchar *format, ...);
+
+gboolean skype_debug_cb(SkypeDebugWrapper *wrapper);
+
+void
+skype_debug_info(const gchar *category, const gchar *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	skype_debug_vargs(PURPLE_DEBUG_INFO, category, format, args);
+	va_end(args);
+}
+
+void
+skype_debug_warning(const gchar *category, const gchar *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	skype_debug_vargs(PURPLE_DEBUG_WARNING, category, format, args);
+	va_end(args);
+}
+
+void
+skype_debug_error(const gchar *category, const gchar *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	skype_debug_vargs(PURPLE_DEBUG_ERROR, category, format, args);
+	va_end(args);
+}
+
+void
+skype_debug(PurpleDebugLevel level, const char *category,
+				const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+	skype_debug_vargs(level, category, format, args);
+	va_end(args);
+}
+
+void
+skype_debug_vargs(PurpleDebugLevel level, const char *category,
+				 const char *format, va_list args)
+{
+	SkypeDebugWrapper *wrapper;
+	
+	wrapper = g_new(SkypeDebugWrapper, 1);
+	wrapper->level = level;
+	wrapper->category = g_strdup(category);
+	wrapper->message = g_strdup_vprintf(format, args);
+	
+	purple_timeout_add(1, (GSourceFunc) skype_debug_cb, (gpointer)wrapper);
+}
+
+gboolean
+skype_debug_cb(SkypeDebugWrapper *wrapper)
+{
+	if (wrapper != NULL)
+		purple_debug(wrapper->level, wrapper->category, wrapper->message);
+	return FALSE;
+}
