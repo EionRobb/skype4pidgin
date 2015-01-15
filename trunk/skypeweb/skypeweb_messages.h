da229241c73fe3bb109db1caa7bb161c6a5e538a
eionrobb <eionrobb@a094effa-da3d-0410-9f80-1db20ef89389>
2015-01-15 03:05:40 +0000

    SkypeWeb : Display users in a group chat when rejoining a room or getting info from an already joined room

--- trunk/skypeweb/skypeweb_messages.h
+++ trunk/skypeweb/skypeweb_messages.h
@@ -18,6 +18,7 @@ void skypeweb_initiate_chat_from_node(PurpleBlistNode *node, gpointer userdata);
 void skypeweb_subscribe_to_contact_status(SkypeWebAccount *sa, GSList *contacts);
 void skypeweb_get_conversation_history_since(SkypeWebAccount *sa, const gchar *convname, time_t since);
 void skypeweb_get_conversation_history(SkypeWebAccount *sa, const gchar *convname);
+void skypeweb_get_thread_users(SkypeWebAccount *sa, const gchar *convname);
 void skypeweb_get_all_conversations_since(SkypeWebAccount *sa, time_t since);
 void skype_web_get_offline_history(SkypeWebAccount *sa);
 
