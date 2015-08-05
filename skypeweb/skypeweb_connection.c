0c354d57caf1f279bbeadf80a3a57cfda3cb6217
Eion Robb <eion@robbmob.com>
2015-08-06 08:51:10 +1200

    Skypeweb : Ugh, forgot to commit this in previous commits too :(

--- skypeweb/skypeweb_connection.c
+++ skypeweb/skypeweb_connection.c
@@ -583,7 +583,7 @@ skypeweb_post_or_get(SkypeWebAccount *sa, SkypeWebMethod method,
 		g_string_append_printf(request, "Content-length: %zu\r\n", strlen(postdata));
 	}
 	
-	if (g_str_equal(host, SKYPEWEB_CONTACTS_HOST) || g_str_equal(host, SKYPEWEB_VIDEOMAIL_HOST)) {
+	if (g_str_equal(host, SKYPEWEB_CONTACTS_HOST) || g_str_equal(host, SKYPEWEB_VIDEOMAIL_HOST) || g_str_equal(host, SKYPEWEB_NEW_CONTACTS_HOST)) {
 		g_string_append_printf(request, "X-Skypetoken: %s\r\n", sa->skype_token);
 		g_string_append(request, "X-Stratus-Caller: " SKYPEWEB_CLIENTINFO_NAME "\r\n");
 		g_string_append(request, "X-Stratus-Request: abcd1234\r\n");
