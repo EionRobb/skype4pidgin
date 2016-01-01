c82540f47c05dad844556f910d387502907392ad
Eion Robb <eion@robbmob.com>
2016-01-02 12:14:48 +1300

    SkypeWeb : Don't provide api already available in 2.11

--- skypeweb/libskypeweb.h
+++ skypeweb/libskypeweb.h
@@ -175,6 +175,7 @@
 	#define purple_hash_destroy purple_cipher_context_destroy
 	#define purple_xfer_set_protocol_data(xfer, proto_data) ((xfer)->data = (proto_data))
 	#define purple_xfer_get_protocol_data(xfer) ((xfer)->data)
+#if !PURPLE_VERSION_CHECK(2, 11, 0)
 static inline gboolean
 purple_xfer_write_file(PurpleXfer *xfer, const guchar *buffer, gsize size) {
 	PurpleXferUiOps *ui_ops = purple_xfer_get_ui_ops(xfer);
@@ -182,6 +183,7 @@ purple_xfer_write_file(PurpleXfer *xfer, const guchar *buffer, gsize size) {
 		(ui_ops && ui_ops->ui_write ? ui_ops->ui_write(xfer, buffer, size) : fwrite(buffer, 1, size, xfer->dest_fp)));
 	return TRUE;
 }
+#endif
 	#define PURPLE_CMD_FLAG_PROTOCOL_ONLY PURPLE_CMD_FLAG_PRPL_ONLY
 	#define PURPLE_CMD_P_PLUGIN PURPLE_CMD_P_PRPL
 	
